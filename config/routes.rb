8cc4abcdf19a7d7b614087f0e46b34e2e43599c8
EC2 Default User <ec2-user@ip-172-31-47-7.ap-northeast-1.compute.internal>
2023-01-21 10:06:42 +0000

    [finnish]bookの新規登録機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'books/show' => 'books#show'
   get 'books/edit' => 'books#edit'
   get 'homes/top' => 'homes#top'
+  post 'books' => 'books#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
