a8122ddda39819b81a706921e8ac66bfa1d6274f
EC2 Default User <ec2-user@ip-172-31-47-7.ap-northeast-1.compute.internal>
2023-01-22 12:28:45 +0000

    [add]フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,18 +9,26 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id]) 
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books/index'
+    flash[:notice] = "Book was successfully created"
+    redirect_to book_path(book.id)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
